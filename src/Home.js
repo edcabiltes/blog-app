ac6e686c6d0fc14aab724487f2dbd9f651d1fa27
Ed Cabiltes <edcabiltes@outlook.com>
2025-01-20 06:32:36 +0800

    Integrated useEffect hook

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Bloglist from "./Bloglist";
 
 const Home = () => {
@@ -12,6 +12,11 @@ const Home = () => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
+
+    useEffect(() => {
+        console.log('use effect ran');
+        console.log(blogs);
+    })
 
     return (
         <div className="home">
