19ce6a4c83d5f761ebd14194d3329c23a8cb8856
Ed Cabiltes <edcabiltes@outlook.com>
2025-01-20 04:21:26 +0800

    Implemented component reuse

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,13 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My project', body: 'lorem ipsum...', author: 'ed', id: 1 },
         { title: 'My book collection', body: 'lorem ipsum...', author: 'baste', id: 2 },
-        { title: 'What I learned', body: 'lorem ipsum...', author: 'makoy', id: 3 }
+        { title: 'What I learned', body: 'lorem ipsum...', author: 'ed', id: 3 }
     ]);
 
     return (
         <div className="home">
             <Bloglist blogs={blogs} title="All Blogs" />
+            <Bloglist blogs={blogs.filter(blog => blog.author === 'ed')} title="Ed's Blogs" />
         </div>
     )
 }   
