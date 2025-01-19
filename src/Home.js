a15d9fd244a39e95b3b42c54db958c88a2d574cc
Ed Cabiltes <edcabiltes@outlook.com>
2025-01-19 13:35:21 +0800

    Added click event to log messages in the console

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,18 @@
 const Home = () => {
-  return (
-    <div className="home">
-        <h2>Homepage</h2>
-    </div>
-  )
-}
+    const handleClick = (e) => {
+        console.log('hello', e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name, e.target);
+    }
+
+    return (
+        <div className="home">
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('ed', e)}>Click me again</button>
+        </div>
+    )
+}   
 
 export default Home
