f1aef52e7213c7ead8e0494e35afd224588f2f8c
Ed Cabiltes <edcabiltes@outlook.com>
2025-01-22 17:37:39 +0800

    Added conditional loading message

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import Bloglist from "./Bloglist";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     const [name, setName] = useState('ed');
 
@@ -12,16 +13,21 @@ const Home = () => {
     }
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
             .then(res => {
                 return res.json();
-            }).then(data => {
+            })
+            .then(data => {
+                setIsPending(false);
                 setBlogs(data);
             })
+        }, 1000);
     }, []);
 
     return (
         <div className="home">
+            { isPending && <div>Loading...</div>}
             {blogs && <Bloglist blogs={blogs} />}
         </div>
     )
