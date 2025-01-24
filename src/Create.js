df9b23fd28976d8686260847c6ce5e22dc0a36af
Ed Cabiltes <edcabiltes@outlook.com>
2025-01-24 14:41:31 +0800

    Added POST request

--- src/Create.js
+++ src/Create.js
@@ -4,12 +4,22 @@ const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('ed');
+  const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
 
-    console.log(blog);
+    setIsPending(true);
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('new blog added');
+      setIsPending(false);
+    })
   }
 
   return (
@@ -37,7 +47,8 @@ const Create = () => {
             <option value='ed'>ed</option>
             <option value='baste'>baste</option>
           </select>
-          <button>Add Blog</button>
+          { !isPending && <button>Add Blog</button> }
+          { isPending && <button disabled>Adding Blog...</button> }
         </form>
     </div>
   )
